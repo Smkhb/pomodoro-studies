ddab75dfe6428c4f2435fe0f033a5c9f1d5ba0ef
SAMUEL BERNARDO <samuel.hagers.bernardo@gmail.com>
2023-05-02 15:48:34 -0300

    state no formulario

--- src/components/Fomulario/index.tsx
+++ src/components/Fomulario/index.tsx
@@ -3,21 +3,48 @@ import Botao from "../Botao";
 import style from './Formulario.module.scss';
 
 class Formulario extends React.Component {
-    render () {
+    state = {
+        tarefa: '',
+        tempo: '00:00'
+    }
+    addTarefa(e:React.FormEvent) {
+        e.preventDefault()
+        console.log('state:', this.state)
+    }
+
+    render() {
         return (
-            <form className={style.novaTarefa}>
+            <form className={style.novaTarefa} onSubmit={this.addTarefa.bind(this)}>
                 <div className={style.inputContainer}>
                     <label htmlFor="tarefa">
                         Adicione um novo estudo
                     </label>
-                    <input type="text" name="tarefa" id="tarefa" placeholder="O que você quer estudar?" required />
+                    <input
+                        type="text"
+                        name="tarefa"
+                        id="tarefa"
+                        placeholder="O que você quer estudar?"
+                        required
+                        value={this.state.tarefa}
+                        onChange={e => this.setState({ ...this.state, tarefa: e.target.value })}
+                    />
                 </div>
-                
+
                 <div className={style.inputContainer}>
                     <label htmlFor="tempo">
-                        Tempo 
+                        Tempo
                     </label>
-                    <input type="time" step='1' name="tempo" id="tempo" min='00:00:00' max='01:30:00' required />
+                    <input
+                        type="time"
+                        step='1'
+                        name="tempo"
+                        id="tempo"
+                        min='00:00:00'
+                        max='01:30:00'
+                        required
+                        value={this.state.tempo}
+                        onChange={e => this.setState({ ...this.state, tempo: e.target.value })}
+                    />
                 </div>
                 <Botao>Adicionar</Botao>
             </form>
