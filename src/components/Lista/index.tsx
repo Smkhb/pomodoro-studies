e212831209ba45bad32a2509746ad38ddc86f364
SAMUEL BERNARDO <samuel.hagers.bernardo@gmail.com>
2023-05-02 12:03:55 -0300

    Lista de Estudos

---
+++ src/components/Lista/index.tsx
@@ -0,0 +1,19 @@
+
+export default function Lista() {
+  return (
+    <aside>
+        <h2>Estudos do Dia</h2>
+        <ul>
+            <li>
+               <h3>React.js</h3>
+               <span>01:00:00</span> 
+            </li>
+            <li>
+                <h3>MongoDB</h3>
+                <span>01:30:00</span>
+            </li>
+        </ul>
+    </aside>
+  )
+}
+
--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,12 @@ import './main.css'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import Formulario from './components/Fomulario'
+import Lista from './components/Lista'
 
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <Formulario />
+    <Lista/>
   </React.StrictMode>
 )
